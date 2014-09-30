7d8e545ee832ebb0781b780ffb5846f40bc13098
mahmoud <inbox@mahmoudzalt.com>
2014-09-30 16:47:02 +0300

    fix the dynamic subsections injection

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,32 +5,25 @@ $(function () {
     // inject the required fields in the html on runtime
     function setup()
     {
-        inject("genre");
-        inject("product");
-        inject("artist");
-        inject("track");
+        inject("genre", null);
+        inject("product", null);
+        inject("artist", null);
+        inject("track", null);
     }
 
     // listen to duplicable buttons
     jQuery(document.body).on('click','.duplicable-btn',function(e)
     {
         var section = $(this).attr('section');
+        var num = $(this).attr('num');
+
         // inject the selected section
-        inject(section);
+        inject(section, num);
 
     });
 
-    // listen to track duplicable buttons
-    jQuery(document.body).on('click','.track-duplicable-btn',function(e)
-    {
-        var section = $(this).attr('section');
-        // inject the selected section
-        track_inject(section);
-
-    });
-
-    // the function injects a field
-    function inject(section)
+    // the function injects a field | the num param is important for the track subsections
+    function inject(section, num)
     {
         // get the template source from the server
         getTemplateSource(section, function(source)
@@ -38,10 +31,17 @@ $(function () {
             // compile the template with Handlebars
             var template = Handlebars.compile(source);
 
-            // count how many of this template already exist in the HTML
-            var counter = $('.' + section).length;
             // the track_count is usefull for the track subsections
             var track_count = $('.track').length;
+
+            if(section == 'track-genre')
+            {
+                // count how many of this template already exist in the HTML
+                var counter = $('.' + section + '-' + num).length;
+            }else{
+                // count how many of this template already exist in the HTML
+                var counter = $('.' + section).length;
+            }
 
             // if section is 'track' then only the track_count is important else the absolute opposite
             var context = {count: counter, track_count: track_count};
@@ -49,45 +49,21 @@ $(function () {
             // build the final html
             var field = template(context);
 
-            // append the field template to the html page
-            $("."+section+"-group").append(field);
-
+            if(section == 'track-genre')
+            {
+                // append the field template to the html page
+                $("."+section+"-group-"+num).append(field);
+            }else
+            {
+                // append the field template to the html page
+                $("."+section+"-group").append(field);
+            }
             // if is the track section, get (inject) its sub sections also
             if(section == 'track')
             {
-                track_inject("track-genre");
-  //            inject("track-product", null);
+                inject("track-genre", track_count);
             }
-
-
         });
-    }
-
-
-    // the function injects a field
-    function track_inject(section)
-    {
-        // count the tracks to figure out the group id
-        var track_count = $('.track').length;
-
-        // get the internal template source
-        var source = $("#"+section+"-"+track_count).html();
-        console.log($("#"+section+"-"+track_count));
-        alert($("#"+section+"-"+track_count));
-        // compile the template with Handlebars
-        var template = Handlebars.compile(source);
-
-        // count how many of this template already exist in the HTML
-        var counter = $('.' + section).length;
-
-        // if section is 'track' then only the track_count is important else the absolute opposite
-        var context = {count: counter, track_count: track_count};
-
-        // build the final html
-        var field = template(context);
-
-        // append the field template to the html page
-        $("."+section+"-group").append(field);
     }
 
 
